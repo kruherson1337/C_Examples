e395494498edb8d35bbfdf0f0fdffe4e41503dbe
Tomaž Stoiljković <tomaz.stoiljkovic@student.um.si>
2019-05-22 11:08:27 +0200

    Added examples

---
+++ 1_Swap/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(char* a, char* b) {
+    char c = *b;
+    *b = *a;
+    *a = c;
+}
+
+int main() {
+    printf("Hello, World!\n");
+
+    char a = 'a';
+    char b = 'b';
+
+    swap(&a, &b);
+
+    printf("a: %c\n", a);
+    printf("b: %c", b);
+
+
+    return 0;
+}
